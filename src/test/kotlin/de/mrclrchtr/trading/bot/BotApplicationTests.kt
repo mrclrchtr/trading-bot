0ebb2763cd68aa8eac023d3788cae1c51c7cab2b
Marcel Richter <marcel.richter89@gmail.com>
2019-06-08 12:11:12 +0200

    first commit

---
+++ src/test/kotlin/de/mrclrchtr/trading/bot/BotApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.mrclrchtr.trading.bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
